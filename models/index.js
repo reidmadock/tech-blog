9aedd10ee0a7f948974581e1e9c5892c4f2fd691
Reid M <reidmadock@gmail.com>
2023-05-23 14:01:01 -0500

    Added comments to model associations

--- models/index.js
+++ models/index.js
@@ -2,19 +2,28 @@ const User = require('./User');
 const UserPost = require('./UserPost');
 const Comment = require('./Comment');
 
+/* Add user_id foreign key to a UserPost
+ Every post has one associated User */
 UserPost.belongsTo(User, {
     foreignKey: "user_id"
 });
 
+/* Add user_post_id foreign key to a Comment
+ Comments become associated to a specific post */
 Comment.belongsTo(UserPost, {
     foreignKey: "user_post_id"
 });
 
+/* Adds user_post_id foreign key to Comment
+ Along with the plural association
+ so every post can have multiple comments */
 UserPost.hasMany(Comment, {
     foreignKey: "user_post_id",
     onDelete: "CASCADE"
 });
 
+/* Add user_id foreign key to Comment
+ Every Comment has one associated User */
 Comment.belongsTo(User, {
     foreignKey: "user_id"
 })
