6b5a4806bf9f957c90acee5d11ea9d5762c464e8
Reid M <reidmadock@gmail.com>
2023-06-01 13:55:39 -0500

    style mostly done

--- models/Comment.js
+++ models/Comment.js
@@ -31,6 +31,17 @@ Comment.init(
         },
     },
     {
+        hooks: {
+            beforeCreate : (record, options) => {
+                record.dataValues.createdAt = new Date().toISOString().replace(/T/, ' ').replace(/\..+/g, '');
+                console.log(`Before Create Hook DataVal Created at:${record.dataValues.createdAt}`)
+                // record.dataValues.updatedAt = new Date().toISOString().replace(/T/, ' ').replace(/\..+/g, '');
+            },
+            afterCreate : (record, options) => {
+                record.dataValues.createdAt = new Date().toISOString().replace(/T/, ' ').replace(/\..+/g, '');
+                console.log(`After Create DataVal Created at:${record.dataValues.createdAt}`)
+            },
+        },
         sequelize,
         timestamps: true, // Should I be using timestamps true? Or just add a data field...
         updatedAt: false,
